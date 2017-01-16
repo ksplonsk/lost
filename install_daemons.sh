d902aaa2e135d6699b1cb1e058ba55c864d6366c
Kristen Splonskowski <ksplons@atlassian.com>
2017-01-16 13:20:00 -0800

    fixed curl command

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,12 +1,19 @@
+#!/bin/bash
+
 #git clone https://git.postgresql.org/git/postgresql.git
 #cd postgresql
-#./configure 
+#./configure --prefix=$1
 #make
 #make install
-curl http://mirrors.koehn.com/apache//httpd/httpd-2.4.25.tar.gz > httpd-2.4.25.tar.gz
+
+#cd ..
+
+curl -o  httpd-2.4.25.tar.gz http://mirrors.koehn.com/apache//httpd/httpd-2.4.25.tar.gz
 tar -xvzf httpd-2.4.25.tar.gz
 cd httpd-2.4.25
-./configure --prefix=~/apache2
+./configure --prefix=$1
 make
 make install
 # TODO: need to configure for port 8080
+
+cd ..
