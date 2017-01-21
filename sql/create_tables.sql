31d3c0615c593e33b29a6afdcca160cdc6da8736
Kristen Splonskowski <ksplons@atlassian.com>
2017-01-21 15:44:37 -0800

    added the rest of the CREATE TABLE commands

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -28,6 +28,76 @@ location varchar(255)
 CREATE TABLE asset_at (
 asset_fk int,
 facility_fk int,
-arrive_dt int,
-depart_dt int
+arrive_dt timestamp,
+depart_dt timestamp
 );
+
+CREATE TABLE convoys (
+convoy_pk int,
+request varchar(255),
+source_fk int,
+dest_fk int,
+depart_dt timestamp, 
+arrive_dt timestamp 
+);
+
+CREATE TABLE used_by (
+vehicle_fk int,
+convoy_fk int
+);
+
+CREATE TABLE asset_on (
+asset_fk int,
+convoy_fk int,
+load_dt timestamp, 
+unload_dt timestamp 
+);
+
+CREATE TABLE users (
+user_pk int,
+username varchar(255),
+active boolean
+);
+
+CREATE TABLE roles (
+role_pk int,
+title varchar(255)
+);
+
+CREATE TABLE user_is (
+user_fk int,
+role_fk int
+);
+
+CREATE TABLE user_supports (
+user_fk int,
+facility_fk int
+);
+
+CREATE TABLE level (
+level_pk int,
+abbrv varchar(255),
+comment varchar(255)
+);
+
+CREATE TABLE compartments (
+compartment_pk int,
+abbrv varchar(255),
+comment varchar(255)
+);
+
+CREATE TABLE security_tags (
+tag_pk int,
+level_fk int,
+compartment_fk int,
+user_fk int,
+product_fk int,
+asset_fk int
+);
+
+
+
+
+
+
+
