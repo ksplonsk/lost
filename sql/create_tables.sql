bef1bc6b8da8799197360b95414831b20294129f
Kristen Splonskowski <ksplons@atlassian.com>
2017-01-21 16:31:35 -0800

    fixed some of the CREATE TABLE commands

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,98 +1,98 @@
 CREATE TABLE products (
-product_pk int,
+product_pk serial primary key,
 vendor varchar(255),
 description varchar(255),
 alt_description varchar(255)
 );
 
 CREATE TABLE assets (
-asset_pk int,
-product_fk int,
+asset_pk serial primary key,
+product_fk integer REFERENCES products(product_pk) not null,
 asset_tag varchar(255),
 description varchar(255),
 alt_description varchar(255)
 );
 
 CREATE TABLE vehicles (
-vehicle_pk int,
-asset_fk int
+vehicle_pk serial primary key,
+asset_fk integer REFERENCES assets(asset_pk) not null
 );
 
 CREATE TABLE facilities (
-facility_pk int,
+facility_pk serial primary key,
 fcode varchar(255),
 common_name varchar(255),
 location varchar(255)
 );
 
 CREATE TABLE asset_at (
-asset_fk int,
-facility_fk int,
+asset_fk integer REFERENCES assets(asset_pk) not null,
+facility_fk integer REFERENCES facilities(facility_pk) not null,
 arrive_dt timestamp,
 depart_dt timestamp
 );
 
 CREATE TABLE convoys (
-convoy_pk int,
+convoy_pk serial primary key,
 request varchar(255),
-source_fk int,
-dest_fk int,
+source_fk integer REFERENCES facilities(facility_pk) not null,
+dest_fk integer REFERENCES facilities(facility_pk) not null,
 depart_dt timestamp, 
 arrive_dt timestamp 
 );
 
 CREATE TABLE used_by (
-vehicle_fk int,
-convoy_fk int
+vehicle_fk integer REFERENCES vehicles(vehicle_pk) not null,
+convoy_fk integer REFERENCES convoys(convoy_pk) not null
 );
 
 CREATE TABLE asset_on (
-asset_fk int,
-convoy_fk int,
+asset_fk integer REFERENCES assets(asset_pk) not null,
+convoy_fk integer REFERENCES convoys(convoy_pk) not null,
 load_dt timestamp, 
 unload_dt timestamp 
 );
 
 CREATE TABLE users (
-user_pk int,
+user_pk serial primary key,
 username varchar(255),
 active boolean
 );
 
 CREATE TABLE roles (
-role_pk int,
+role_pk serial primary key,
 title varchar(255)
 );
 
 CREATE TABLE user_is (
-user_fk int,
-role_fk int
+user_fk integer REFERENCES users(user_pk) not null,
+role_fk integer REFERENCES roles(role_pk) not null
 );
 
 CREATE TABLE user_supports (
-user_fk int,
-facility_fk int
+user_fk integer REFERENCES users(user_pk) not null,
+facility_fk integer REFERENCES facilities(facility_pk) not null
 );
 
 CREATE TABLE level (
-level_pk int,
+level_pk serial primary key,
 abbrv varchar(255),
 comment varchar(255)
 );
 
 CREATE TABLE compartments (
-compartment_pk int,
+compartment_pk serial primary key,
 abbrv varchar(255),
 comment varchar(255)
 );
 
 CREATE TABLE security_tags (
-tag_pk int,
-level_fk int,
-compartment_fk int,
-user_fk int,
-product_fk int,
-asset_fk int
+tag_pk serial primary key,
+level_fk integer REFERENCES level(level_pk) not null,
+compartment_fk integer REFERENCES compartments(compartment_pk) not null,
+user_fk integer REFERENCES users(user_pk),
+product_fk integer REFERENCES products(product_pk),
+asset_fk integer REFERENCES assets(asset_pk)
 );
 
 
