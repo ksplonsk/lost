9a7ff755d52a79e37d4cbc7ca82bdcbf849c4a97
Kristen Splonskowski <ksplons@atlassian.com>
2017-01-22 16:14:50 -0800

    fixed python command by adding arguments

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -1,5 +1,5 @@
 curl -o osnap_legacy.tar.gz https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
 tar -xvzf osnap_legacy.tar.gz
-python import_data.py
+python import_data.py $1 $2
 rm osnap_legacy.tar.gz
 rm -r osnap_legacy
