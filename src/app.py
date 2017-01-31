2822ddef026b746ec1b9b0ba5fdf762292563272
Kristen Splonskowski <ksplons@atlassian.com>
2017-01-31 13:34:16 -0800

    fixed app.py

--- src/app.py
+++ src/app.py
@@ -4,18 +4,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html')
+	return render_template('welcome.html')
 
 @app.route('/goodbye')
 def goodbye():
-    if request.method=='GET' and 'mytext' in request.args:
-        return render_template('goodbye.html',data=request.args.get('mytext'))
+	if request.method=='GET' and 'mytext' in request.args:
+		return render_template('goodbye.html',data=request.args.get('mytext'))
 
-    # request.form is only populated for POST messages
-    if request.method=='POST' and 'mytext' in request.form:
-        return render_template('goodbye.html',data=request.form['mytext'])
-    return render_template('index.html')
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+@app.route('/report_menu')
+def report_menu():
+	return render_template('report_menu.html')
+
+@app.route('/facility_inventory_report')
+def facility_inventory_report():
+	return render_template('facility_inventory_report.html')
+
+@app.route('/in_transit_report')
+def in_transit_report():
+	return render_template('in_transit_report.html')
+
+@app.route('/logout')
+def logout():
+	return render_template('logout.html')
+
+
+	# request.form is only populated for POST messages
+	if request.method=='POST' and 'mytext' in request.form:
+		return render_template('goodbye.html',data=request.form['mytext'])
+	return render_template('index.html')
