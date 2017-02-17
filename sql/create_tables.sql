c4729722fdeb8fde147a56148f67f7f639298fe3
Kristen Splonskowski <ksplons@atlassian.com>
2017-02-17 13:11:35 -0800

    new preflight.sh and create_tables.sql

---
+++ sql/create_tables.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+user_pk serial primary key, # chose to use a numeric pk, I am more comfortable using numeric pk's
+username varchar(16), # length of 16 characters specified in project specs
+password varchar(16), # length of 16 characters specified in project specs
+);
