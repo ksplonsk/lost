74959db00f0b199878e3b5cbddfab0fde9added2
Kristen Splonskowski <ksplons@atlassian.com>
2017-02-19 14:55:31 -0800

    fixed error in create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users (
-# chose to use a numeric pk, I am more comfortable using numeric pk's
+-- chose to use a numeric pk, I am more comfortable using numeric pk's
 user_pk serial primary key,
 
- # length of 16 characters specified in project specs
+-- length of 16 characters specified in project specs
 username varchar(16),
 
-# length of 16 characters specified in project specs
+-- length of 16 characters specified in project specs
 password varchar(16), 
 );
