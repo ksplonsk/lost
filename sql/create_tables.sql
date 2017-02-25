a4de79c6ae0adbd2648590bfb97c6f6893bca7e0
Kristen Splonskowski <ksplons@atlassian.com>
2017-02-24 16:16:45 -0800

    updated create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,24 +3,27 @@ role_pk serial primary key, -- chose to use a numeric pk, I am more comfortable 
 title varchar(32) -- length of 16 characters specified in project specs
 );
 
+INSERT INTO roles (title) VALUES ('Logistics Officer');
+
 CREATE TABLE users (
 user_pk serial primary key, -- chose to use a numeric pk, I am more comfortable using numeric pk's
-role_fk integer REFERENCES roles(role_pk) not null,
+role_fk integer REFERENCES roles(role_pk) not null, -- connects roles to users
 username varchar(16), -- length of 16 characters specified in project specs
 password varchar(16) -- length of 16 characters specified in project specs
 );
 
 CREATE TABLE assets (
-asset_pk serial primary key,
-asset_tag varchar(16),
-description varchar(255)
+asset_pk serial primary key, -- chose to use a numeric pk, I am more comfortable using numeric pk's
+facility_fk integer REFERENCES facilities(facility_pk), -- connects facilities assets
+asset_tag varchar(16), -- tag for the asset
+description varchar(255) -- description of the asset
 );
 
 CREATE TABLE facilities (
-facility_pk serial primary key,
-fcode varchar(6),
-common_name varchar(32),
-location varchar(255)
+facility_pk serial primary key, -- chose to use a numeric pk, I am more comfortable using numeric pk's
+fcode varchar(6), -- facility code
+common_name varchar(32), -- common name for the facility
+location varchar(255) -- location of the facility
 );
 
 
