4fecb2b2385991a560737434ee8e617d28bb0a19
Kristen Splonskowski <ksplons@atlassian.com>
2017-02-24 16:01:25 -0800

    updated create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,5 +1,28 @@
+CREATE TABLE roles (
+role_pk serial primary key, -- chose to use a numeric pk, I am more comfortable using numeric pk's
+title varchar(32) -- length of 16 characters specified in project specs
+);
+
 CREATE TABLE users (
 user_pk serial primary key, -- chose to use a numeric pk, I am more comfortable using numeric pk's
+role_fk integer REFERENCES roles(role_pk) not null,
 username varchar(16), -- length of 16 characters specified in project specs
 password varchar(16) -- length of 16 characters specified in project specs
 );
+
+CREATE TABLE assets (
+asset_pk serial primary key,
+asset_tag varchar(16),
+description varchar(255)
+);
+
+CREATE TABLE facilities (
+facility_pk serial primary key,
+fcode varchar(6),
+common_name varchar(32),
+location varchar(255)
+);
+
+
+
+
