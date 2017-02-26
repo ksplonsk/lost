d366312ab3e9ece5a707d9ccdcc11c14cd141b26
Kristen Splonskowski <ksplons@atlassian.com>
2017-02-26 12:16:11 -0800

    modified app.py

--- src/app.py
+++ src/app.py
@@ -105,28 +105,26 @@ def add_facility():
 		# 	role_options.append(role[0])
 		return render_template('add_facility.html')
 
-	if request.method=='POST' and 'username' in request.form and 'password' in request.form:
-		username = request.form['username']
-		password = request.form['password']
+	if request.method=='POST' and 'facility_name' in request.form and 'fcode' in request.form and 'location' in request.form:
+		facility_name = request.form['facility_name']
+		fcode = request.form['fcode']
+		location = request.form['location']
 
 		conn = psycopg2.connect(dbname=dbname, host=dbhost,port=dbport)
 		cur = conn.cursor()
 
-		SQL = "SELECT * FROM users WHERE username=%s;"
-		cur.execute(SQL, (username,))
-		user = cur.fetchone()
+		SQL = "SELECT * FROM facilities WHERE facility_name=%s OR fcode=%s;"
+		cur.execute(SQL, (facility_name, fcode))
+		facility = cur.fetchone()
 
-		# if no user by that name, go to unmatched page
-		if user == None:
-			return render_template('unmatched.html', username=username)
+		# if facility already exists, go to facility already exists page
+		if facility != None:
+			return render_template('facility_already_exists.html', facility_name=facility_name, fcode=fcode)
 
-		# if username and password don't exist, go back to unmatched page
-		if user[2] == password:
-			session['username'] = username
-			session['logged_in'] = True
-			return redirect(url_for('dashboard'))
-
-		else:
-			return render_template('unmatched.html', username=username)
+		SQL = "INSERT INTO facilities (facility_pk, common_name, fcode, location) VALUES (DEFAULT, %s, %s, %s);"
+		cur.execute(SQL, (facility_name,fcode,location))
+		conn.commit()
+		
+		return redirect(url_for('add_facility'))
 
 
