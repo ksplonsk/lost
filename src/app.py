9b86d4fb2007d383fb2c1db4d8d8ee66705dfada
Kristen Splonskowski <ksplons@atlassian.com>
2017-03-04 16:09:38 -0800

    added code to dashboard to show transfer reqs needing approval

--- src/app.py
+++ src/app.py
@@ -403,7 +403,7 @@ def approve_req():
 		cur = conn.cursor()
 
 		if request.form['approve'] != None:
-			SQL = "UPDATE transfers SET approver_fk=(SELECT user_pk FROM users WHERE username=%s), approver_dt=CURRENT_TIMESTAMP WHERE (asset_fk=(SELECT asset_pk FROM assets WHERE asset_tag=%s))"
+			SQL = "UPDATE transfers SET approver_fk=(SELECT user_pk FROM users WHERE username=%s), approved_dt=CURRENT_TIMESTAMP WHERE (asset_fk=(SELECT asset_pk FROM assets WHERE asset_tag=%s))"
 			cur.execute(SQL, (session['username'], asset_tag))
 			conn.commit()
 
