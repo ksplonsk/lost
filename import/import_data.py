c62b81a63a6e283efdac65e7c6bb5ff8f91b067f
Kristen Splonskowski <ksplons@atlassian.com>
2017-03-10 13:22:40 -0800

    fixes

--- import/import_data.py
+++ import/import_data.py
@@ -16,3 +16,9 @@ with open(sys.argv[2]+'facilities.csv') as csvfile:
 	for row in rows:
 		cur.execute("INSERT INTO facilities (fcode, common_name) VALUES (%s, %s)", (row['fcode'], row['common_name']))
 	conn.commit()
+
+with open(sys.argv[2]+'assets.csv') as csvfile:
+	rows = csv.DictReader(csvfile)
+	for row in rows:
+		cur.execute("INSERT INTO assets (asset_tag, description, facility, acquired, disposed) VALUES ((SELECT role_pk FROM roles WHERE title=%s), %s, %s, %s)", (row['role'], row['username'], row['password'], row['active']))
+	conn.commit()
