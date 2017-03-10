cc7b7dc4226887c16a1e043759c2ee54ca2c76b6
Kristen Splonskowski <ksplons@atlassian.com>
2017-03-10 14:10:46 -0800

    adding transfers

--- import/import_data.py
+++ import/import_data.py
@@ -10,6 +10,7 @@ with open(sys.argv[2]+'users.csv') as csvfile:
 
 	for row in rows:
 		cur.execute("INSERT INTO users (role_fk, username, password, active) VALUES ((SELECT role_pk FROM roles WHERE title=%s), %s, %s, %s)", (row['role'], row['username'], row['password'], row['active']))
+	
 	conn.commit()
 
 with open(sys.argv[2]+'facilities.csv') as csvfile:
@@ -17,17 +18,23 @@ with open(sys.argv[2]+'facilities.csv') as csvfile:
 
 	for row in rows:
 		cur.execute("INSERT INTO facilities (fcode, common_name) VALUES (%s, %s)", (row['fcode'], row['common_name']))
+	
 	conn.commit()
 
 with open(sys.argv[2]+'assets.csv') as csvfile:
 	rows = csv.DictReader(csvfile)
 
-	disposed = 't'
 	for row in rows:
+		disposed = 't'
 		if row['disposed'] == 'NULL':
 			disposed = 'f'
 		cur.execute("INSERT INTO assets (asset_tag, description, disposed) VALUES (%s, %s, %s)", (row['asset_tag'], row['description'], disposed))
-		cur.execute("INSERT INTO asset_at (asset_fk, facility_fk, arrival, departure) VALUES ((SELECT asset_pk FROM assets WHERE asset_tag=%s), (SELECT facility_pk FROM facilities WHERE fcode=%s), %s, %s)", (row['facility'], row['acquired'], row['disposed']))
+		
+		departure = row['disposed']
+		if departure == 'NULL':
+			departure = None
+		cur.execute("INSERT INTO asset_at (asset_fk, facility_fk, arrival, departure) VALUES ((SELECT asset_pk FROM assets WHERE asset_tag=%s), (SELECT facility_pk FROM facilities WHERE fcode=%s), %s, %s)", (row['asset_tag'], row['facility'], row['acquired'], departure))
+	
 	conn.commit()
 
 # with open(sys.argv[2]+'transfers.csv') as csvfile:
