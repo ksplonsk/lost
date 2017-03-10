1c3e962d756c83848335a36b28e7635d13fa784f
Kristen Splonskowski <ksplons@atlassian.com>
2017-03-09 18:56:17 -0800

    testing py file

--- export/export_data.py
+++ export/export_data.py
@@ -5,7 +5,7 @@ import psycopg2
 conn = psycopg2.connect(dbname=sys.argv[1], host='127.0.0.1',port=5432)
 cur = conn.cursor()
 
-with open('users.csv', 'wb') as csvfile:
+with open('users.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['username','password', 'role', 'active'])
     
