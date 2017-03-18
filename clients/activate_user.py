497d277c59a7f9f0c390e33602ed6be29fbe47c8
Kristen Splonskowski <ksplons@atlassian.com>
2017-03-18 14:47:00 -0700

    added clients directory

---
+++ clients/activate_user.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def send_request():
+    # add_user
+    # POST http://127.0.0.1:8080/rest/add_user
+
+    try:
+        response = requests.post(
+            url="http://127.0.0.1:8080/rest/add_user",
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+            },
+            data={
+                "arguments": "{\"username\":\"ksplonsk\",\"password\":\"chester\",\"role\":\"Logistics Officer\"}",
+            },
+        )
+        print('Response HTTP Status Code: {status_code}'.format(status_code=response.status_code))
+        print('Response HTTP Response Body: {content}'.format(content=response.content))
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
