fc4be1cbb61d90611bc7605b6635e3306a74c4bd
Kristen Splonskowski <ksplons@atlassian.com>
2017-03-18 14:57:47 -0700

    fixing revoke_user

---
+++ clients/revoke_user.py
@@ -0,0 +1,17 @@
+import requests
+
+try:
+    response = requests.post(
+        url="http://127.0.0.1:8080/rest/revoke_user",
+        headers={
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+        },
+        data={
+            "arguments": "{\"username\":\"malcom\"}",
+        },
+    )
+    print('Response HTTP Status Code: {status_code}'.format(status_code=response.status_code))
+    print('Response HTTP Response Body: {content}'.format(content=response.content))
+except requests.exceptions.RequestException:
+    print('HTTP Request failed')
+
