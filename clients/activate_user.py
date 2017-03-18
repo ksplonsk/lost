51b01437da9e630ae40083b01d23d8eee75c9b91
Kristen Splonskowski <ksplons@atlassian.com>
2017-03-18 15:33:58 -0700

    add parameter passing functionality

--- clients/activate_user.py
+++ clients/activate_user.py
@@ -1,14 +1,20 @@
 import requests
 import sys
+import json
+
+args = dict()
+args['username'] = sys.argv[2]
+args['password'] = sys.argv[3]
+args['role'] = sys.argv[4]
 
 try:
     response = requests.post(
-        url="http://127.0.0.1:8080/rest/activate_user",
+        url="{}rest/activate_user".format(sys.argv[1]),
         headers={
             "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
         },
         data={
-            "arguments": "{\"username\":\"larry\",\"password\":\"chester\",\"role\":\"logofc\"}",
+            "arguments": json.dumps(args),
         },
     )
     print('Response HTTP Status Code: {status_code}'.format(status_code=response.status_code))
